3d9a125492c12fc57a647216c99ba341f5dac4e2
gregleleu <33321678+gregleleu@users.noreply.github.com>
2023-03-03 12:17:40 -0500

    Update dependencies.R
    for tests

--- R/R/dependencies.R
+++ R/R/dependencies.R
@@ -28,7 +28,7 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
   }
 
   packages <- c(
-    "org.datasyslab:geotools-wrapper:1.3.0-27.2",
+    "org.datasyslab:geotools-wrapper:1.4.0-28.2",
     "edu.ucar:cdm-core:5.4.2",
     "org.locationtech.jts:jts-core:1.19.0",
     "org.wololo:jts2geojson:0.16.1"
