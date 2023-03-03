c7e8d9d721e1fba555796d476e1f4bacdb3b4669
gregleleu <33321678+gregleleu@users.noreply.github.com>
2023-03-03 17:22:23 -0500

    test

--- R/tests/testthat.R
+++ R/tests/testthat.R
@@ -27,6 +27,8 @@ if (identical(Sys.getenv("NOT_CRAN"), "true")) {
     spark_disconnect_all()
   })
   
+  print(strsplit(Sys.getenv("SEDONA_JAR_FILES"), ":")[[1]])
+  
   if (!all(file.exists(strsplit(Sys.getenv("SEDONA_JAR_FILES"), ":")[[1]]))) stop("files not found")
 
   filter <- Sys.getenv("TESTTHAT_FILTER", unset = "")
